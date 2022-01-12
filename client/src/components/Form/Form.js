ab907340fb175c5528871d822e882c7453118548
Tanavi Raicar <81321683+traicar@users.noreply.github.com>
2022-01-11 20:33:34 -0600

    file

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import useStyles from './styles'
 import { TextField, Button, Typography, Paper } from '@material-ui/core'
+import FileBase from 'react-file-base64'
 
 const Form = () => {
   const classes = useStyles()
@@ -52,6 +53,13 @@ const Form = () => {
           value={postData.tags}
           onChange={(e) => setPostData({ ...postData, tags: e.target.value })}
         />
+        <div className={classes.fileInput}>
+          <FileBase
+            type="file"
+            multiple={false}
+            onDone={({ base64 }) => setPostData({ ...postData, selectedFile: base64 })}
+          />
+        </div>
       </Form>
     </Paper>
   );
