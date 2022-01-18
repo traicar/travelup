01a755a03cdc0429bbb6855fc0287de58843cd9e
Tanavi Raicar <81321683+traicar@users.noreply.github.com>
2022-01-18 16:15:12 -0600

    delete functionality

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -40,10 +40,8 @@ const Form = ({ currentId, setCurrentId }) => {
   return (
     <Paper className={classes.paper}>
       <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-        <Typography variant="h6">
-          {currentId ? 'Editing' : 'Creating'} a Post
-        </Typography>
-        {/* <Typography variant="h6" > {currentId ? 'Editing' : 'Creating'} a Post</Typography> */}
+
+        <Typography variant="h6" > {currentId ? 'Editing' : 'Creating'} a Post</Typography>
         <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
         <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
         <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })} />
--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -5,9 +5,12 @@ import ThumbUpIcon from '@material-ui/icons/ThumbUpAlt'
 import DeleteIcon from '@material-ui/icons/Delete'
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
 import moment from 'moment'
+import { useDispatch } from 'react-redux'
+import { deletePost } from '../../../actions/posts'
 
 const Post = ({ post, setCurrentId }) => {
   const classes = useStyles()
+  const dispatch = useDispatch()
   return (
     <Card className={classes.card}>
       <CardMedia className={classes.media} image={post.selectedFile} title={post.title}> </CardMedia>
@@ -39,7 +42,8 @@ const Post = ({ post, setCurrentId }) => {
           <ThumbUpIcon fontSize="small" />
           &nbsp; Like &nbsp;{post.likeCount}
         </Button>
-        <Button size="small" color="primary" onClick={() => { }}>
+
+        <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
           <DeleteIcon fontSize="small" />
           Delete
         </Button>
