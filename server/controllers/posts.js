c5cc032222cbd7cb9857d9fd076805e96a8e3fd4
Tanavi Raicar <81321683+traicar@users.noreply.github.com>
2022-01-18 11:32:14 -0600

    post updated syntex

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,33 +1,37 @@
 import PostMessage from "../models/postMessage.js"
-import { Mongoose } from "mongoose";
+import mongoose from 'mongoose'
 
 export const getPosts = async (req, res) => {
   try {
     const postMessages = await PostMessage.find()
-    console.log(postMessages);
-    res.status(200).json(postMessages);
+
+    res.status(200).json(postMessages)
   } catch (error) {
     res.status(404).json({ message: error.message })
   }
 }
 
 export const createPost = async (req, res) => {
-  const body = req.body;
-  const newPost = new PostMessage(body)
+  const post = req.body;
+
+  const newPost = new PostMessage({ ...post, creator: req.userId, createdAt: new Date().toISOString() })
+
   try {
     await newPost.save()
-    res.status(201).json(newPost);
+
+    res.status(201).json(newPost)
   } catch (error) {
-    res.status(404).json({ message: error.message })
+    res.status(409).json({ message: error.message })
   }
 }
 
 export const updatePost = async (req, res) => {
-  const { id: _id } = req.params;
+  const { id: _id } = req.params
   const post = req.body
-  if (!Mongoose.Types.ObjectsId.isValid(_id)) return res.status(404).send('No post with that Id');
 
-  const updatePost = await PostMessage.findByIdAndUpdate(_id, post, { new: true })
+  if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
 
-  res.json(updatePost)
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true })
+
+  res.json(updatedPost)
 }
