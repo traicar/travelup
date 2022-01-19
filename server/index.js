097f716c85562cc82d5585eb98f8d0adbe447e1a
Tanavi Raicar <81321683+traicar@users.noreply.github.com>
2022-01-19 09:20:43 -0600

    env variable server

--- server/index.js
+++ server/index.js
@@ -2,10 +2,13 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js'
 
 const app = express()
+
+dotenv.config()
 
 app.use(bodyParser.json({ limit: "32mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "32mb", extended: true }))
@@ -17,6 +20,6 @@ const CONNECTION_URL = 'mongodb+srv://travelup:GUDishere@cluster0.3louu.mongodb.
 
 const PORT = process.env.PORT || 5000
 
-mongoose.connect(CONNECTION_URL)
+mongoose.connect(process.env.CONNECTION_URL)
   .then(() => app.listen(PORT, () => console.log(`Server running on port:${PORT}`)))
   .catch(err => console.log(err.message))
