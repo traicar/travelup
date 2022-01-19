2d0ff65f6f86e24e29b0d3d7d993a15fa07a1eb5
Tanavi Raicar <81321683+traicar@users.noreply.github.com>
2022-01-19 08:35:08 -0600

    action types to reduce code errors

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,12 +1,12 @@
 import * as api from '../api'
-
+import { FETCH_ALL, DELETE, CREATE, UPDATE } from '../constants/actionTypes';
 //action creators- functions that return actions
 //response data object is fetched from the api and data post is returned it from the backend
 
 export const getPosts = () => async (dispatch) => {
   try {
     const { data } = await api.fetchPosts()
-    dispatch({ type: "FETCH_ALL", payload: data });
+    dispatch({ type: FETCH_ALL, payload: data });
   } catch (error) {
     console.log(error.message)
   }
@@ -16,7 +16,7 @@ export const createPost = (post) => async (dispatch) => {
   try {
     //making api request to backend server to create post
     const { data } = await api.createPost(post)
-    dispatch({ type: "CREATE", payload: data })
+    dispatch({ type: CREATE, payload: data })
   } catch (error) {
     console.log(error.message)
   }
@@ -25,7 +25,7 @@ export const createPost = (post) => async (dispatch) => {
 export const updatePost = (id, post) => async (dispatch) => {
   try {
     const { data } = await api.updatePost(id, post)
-    dispatch({ type: "UPDATE", payload: data })
+    dispatch({ type: UPDATE, payload: data })
   } catch (error) {
     console.log(error.message)
   }
@@ -34,7 +34,7 @@ export const updatePost = (id, post) => async (dispatch) => {
 export const deletePost = (id) => async (dispatch) => {
   try {
     await api.deletePost(id)
-    dispatch({ type: "DELETE", payload: id })
+    dispatch({ type: DELETE, payload: id })
   } catch (error) {
     console.log(error)
   }
