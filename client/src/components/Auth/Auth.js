9440e1ab8df5afc2fe7f95d7f7a933394ebb9870
Tanavi Raicar <81321683+traicar@users.noreply.github.com>
2022-01-19 16:32:45 -0600

    input grid for auth

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,11 +1,41 @@
 import React from 'react'
+import { Avatar, Button, Form, Paper, Grid, Typography, Container, TextField } from '@material-ui/core'
+import useStyles from './styles'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import Input from './Input'
 
 export const Auth = () => {
-  const state = null
+  const classes = useStyles()
+  const isSignup = false
+  const handleSubmit = () => {
+
+  }
+  const handleChange = () => {
+
+  }
   return (
-    <div>
-      AUTH
-    </div>
+    <Container component="main" maxWidth="xs">
+      <Paper className={classes.paper} elevation={3}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography variant="h5">
+          {isSignup ? 'Sign Up' : 'Sign In'}
+        </Typography>
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            {
+              isSignup && (
+                <>
+                  <Input name="firstName" label="First Name" handleChange={handleChange} autoFocus half />
+                  <Input name="lastName" label="Last Name" handleChange={handleChange} half />
+                </>
+              )
+            }
+          </Grid>
+        </form>
+      </Paper>
+    </Container >
   )
 }
 
